aa26975e8b71e6e976070ba0990a79643dcffbad
blaucomfuig <scorcuera6@gmail.com>
2021-06-20 17:05:02 +0200

    image loader database created

---
+++ src/images.js
@@ -0,0 +1,101 @@
+import images from "./imageLoader";
+
+const stockImages = [
+    {
+        id: 1,
+        src: images.allMountain1,
+        tag: "allMountain"
+    },
+    {
+        id: 2,
+        src: images.allMountain2,
+        tag: "allMountain"
+    },
+    {
+        id: 3,
+        src: images.allMountain3,
+        tag: "allMountain"
+    },
+    {
+        id: 4,
+        src: images.allMountain4,
+        tag: "allMountain"
+    },
+    {
+        id: 5,
+        src: images.allMountain5,
+        tag: "allMountain"
+    },
+    {
+        id: 6,
+        src: images.allMountain6,
+        tag: "allMountain"
+    },
+    {
+        id: 7,
+        src: images.allMountain7,
+        tag: "allMountain"
+    },
+    {
+        id: 8,
+        src: images.allMountain8,
+        tag: "allMountain"
+    },
+    {
+        id: 9,
+        src: images.allMountain9,
+        tag: "allMountain"
+    },
+    {
+        id: 10,
+        src: images.allMountain10,
+        tag: "allMountain"
+    },
+    {
+        id: 11,
+        src: images.allMountain11,
+        tag: "allMountain"
+    },
+    {
+        id: 12,
+        src: images.enduro1,
+        tag: "enduro"
+    },
+    {
+        id: 13,
+        src: images.enduro2,
+        tag: "enduro"
+    },
+    {
+        id: 14,
+        src: images.enduro3,
+        tag: "enduro"
+    },
+    {
+        id: 15,
+        src: images.enduro4,
+        tag: "enduro"
+    },
+    {
+        id: 16,
+        src: images.enduro5,
+        tag: "enduro"
+    },
+    {
+        id: 17,
+        src: images.enduro6,
+        tag: "enduro"
+    },
+    {
+        id: 18,
+        src: images.enduro7,
+        tag: "enduro"
+    },
+    {
+        id: 19,
+        src: images.enduro8,
+        tag: "enduro"
+    }
+]
+
+export default stockImages;
