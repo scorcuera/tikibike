9804271fd23b27fefd69f13cc72e752406e2b61a
scorcuera <scorcuera6@gmail.com>
2025-07-21 19:09:33 +0200

    feat: update Contact page layout and styles for improved mobile and desktop responsiveness

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,39 +19,50 @@ const ContactMobile = () => {
   }, []);
 
   return (
-    <div className={classes.big_container}>
-      <div className={classes.container_1}>
-        <div className={classes.section_title}>
-          <h3>Contact</h3>
-        </div>
-        <div className={classes.container_map}>
-          <iframe
-            title="Mapa de ubicación de Tikibike en Vallouise"
-            className={classes.map}
-            src="https://maps.google.com/maps?q=tikibike%20vallouise&t=&z=13&ie=UTF8&iwloc=&output=embed"
-            frameBorder="0"
-            scrolling="no"
-            marginHeight="0"
-            marginWidth="0"
-          />
-        </div>
-        <div className={classes.paragraph_container}>
-          <p className={classes.paragraph_phrase}>
+    <div className={classes.page_container}>
+      <div className={classes.page_title}>
+        <h3>Contact</h3>
+      </div>
+
+      <div className={classes.map_section}>
+        <iframe
+          title="Mapa de ubicación de Tikibike en Vallouise"
+          className={classes.map}
+          src="https://maps.google.com/maps?q=tikibike%20vallouise&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
+          loading="lazy"
+        />
+      </div>
+
+      <div className={classes.info_section}>
+        <div className={classes.location_info}>
+          <p className={classes.location_highlight}>
             À l'entrée de Vallouise en face de la pharmacie
           </p>
-          <p>Le Saint Genest 05290 Vallouise</p>
-          <h6 className={classes.paragraph_title}>
-            Renseignements et réservations :
-          </h6>
-          <p>tikibikevtt@gmail.com</p>
-          <p>06 10 69 01 28 / 06 71 69 53 45</p>
-          <p>GPS : latitude 44,840352. Longitude : 6,494649</p>
-          <p>Horaires d'accueil 8h à 9h– 10 h à 14h – 15 h à 19h</p>
+          <p className={classes.address}>Le Saint Genest 05290 Vallouise</p>
         </div>
-        <div>
-          <div className={classes.container_logo}>
-            <img className={classes.logo_img} src={featuredImages.logo} alt="logo tikibike" />
+
+        <div className={classes.contact_info}>
+          <h4 className={classes.info_title}>Renseignements et réservations</h4>
+          <div className={classes.contact_details}>
+            <span className={classes.contact_text}>
+              📧 tikibikevtt@gmail.com
+            </span>
+            <span className={classes.contact_text}>
+              📱 06 10 69 01 28
+            </span>
+            <span className={classes.contact_text}>
+              📱 06 71 69 53 45
+            </span>
           </div>
+        </div>
+
+        <div className={classes.additional_info}>
+          <p><strong>GPS:</strong> Latitude 44.840352 | Longitude 6.494649</p>
+          <p><strong>Horaires d'accueil:</strong> 8h-9h • 10h-14h • 15h-19h</p>
         </div>
       </div>
     </div>
@@ -59,47 +70,64 @@ const ContactMobile = () => {
 };
 
 const ContactDesktop = () => {
+  // Scroll to top when component mounts
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div className={classes.big_container_big_device}>
-      <div className={classes.section_container}>
-        <div className={classes.section_1}>
-          <div className={classes.section_title}>
-            <h3>Contact</h3>
-          </div>
-          <div className={classes.container_map}>
+    <div className={classes.page_container_desktop}>
+      <div className={classes.page_title}>
+        <h3>Contact</h3>
+      </div>
+
+      <div className={classes.content_wrapper_desktop}>
+        <div className={classes.left_panel}>
+          <div className={classes.map_section_desktop}>
             <iframe
               title="Mapa interactivo de ubicación de Tikibike en Vallouise"
-              className={classes.map}
+              className={classes.map_desktop}
               src="https://maps.google.com/maps?q=tikibike%20vallouise&t=&z=13&ie=UTF8&iwloc=&output=embed"
               frameBorder="0"
               scrolling="no"
               marginHeight="0"
               marginWidth="0"
+              loading="lazy"
             />
           </div>
         </div>
-        <div className={classes.section_2}>
-          <div className={classes.paragraph_container}>
-            <p className={classes.paragraph_phrase}>
-              À l'entrée de Vallouise en face de la pharmacie
-            </p>
-            <p>Le Saint Genest 05290 Vallouise</p>
-            <h6 className={classes.paragraph_title}>
-              Renseignements et réservations :
-            </h6>
-            <p>tikibikevtt@gmail.com</p>
-            <p>06 10 69 01 28 / 06 71 69 53 45</p>
-            <p>GPS : latitude 44,840352. Longitude : 6,494649</p>
-            <p>Horaires d'accueil 8h à 9h– 10 h à 14h – 15 h à 19h</p>
-          </div>
-          <div>
-            <div className={classes.container_logo}>
-              <img className={classes.logo_img} src={featuredImages.logo} alt="logo tikibike" />
+
+        <div className={classes.right_panel}>
+          <div className={classes.info_section_desktop}>
+            <div className={classes.location_info}>
+              <p className={classes.location_highlight}>
+                À l'entrée de Vallouise en face de la pharmacie
+              </p>
+              <p className={classes.address}>Le Saint Genest 05290 Vallouise</p>
+            </div>
+
+            <div className={classes.contact_info}>
+              <h4 className={classes.info_title}>Renseignements et réservations</h4>
+              <div className={classes.contact_details}>
+                <span className={classes.contact_text}>
+                  📧 tikibikevtt@gmail.com
+                </span>
+                <span className={classes.contact_text}>
+                  📱 06 10 69 01 28
+                </span>
+                <span className={classes.contact_text}>
+                  📱 06 71 69 53 45
+                </span>
+              </div>
+            </div>
+
+            <div className={classes.additional_info}>
+              <p><strong>GPS:</strong> Latitude 44.840352 | Longitude 6.494649</p>
+              <p><strong>Horaires d'accueil:</strong> 8h-9h • 10h-14h • 15h-19h</p>
             </div>
           </div>
         </div>
       </div>
-      <div className={classes.footer}></div>
     </div>
   );
 };
